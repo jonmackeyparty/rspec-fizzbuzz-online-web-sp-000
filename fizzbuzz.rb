0054d89771a5c8e92575ac252a3d55c8d1c1d65d
Jon Mackey <jon@jonmackey.party>
2018-10-20 15:35:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(int)
   if int / 3 == 1
     return "Fizz"
-  elsif int % 5 == 1  
+  elsif int / 5 == 1  
     return "Buzz"
-  elsif int % 15 == 1
+  elsif int / 15 == 1
     return "FizzBuzz"
   else 
     return nil 
